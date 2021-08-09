a80d20b3461b802b3ff6f4cd60f7b99399fd7757
hrsh7th <hrsh7th@gmail.com>
2021-08-09 15:05:37 +0900

    Initial commit

---
+++ lua/cmp_emoji/init.lua
@@ -0,0 +1,31 @@
+local source = {}
+
+source.new = function()
+  local self = setmetatable({}, { __index = source })
+  self.items = nil
+  return self
+end
+
+source.get_trigger_characters = function()
+  return { ':' }
+end
+
+source.get_keyword_pattern = function()
+  return [[\%(\s\|^\)\zs:\w*:\?]]
+end
+
+source.complete = function(self, request, callback)
+  -- Avoid unexpected completion.
+  if not vim.regex(self.get_keyword_pattern() .. '$'):match_str(request.context.cursor_before_line) then
+    return callback()
+  end
+
+  if not self.items then
+    self.items = require('cmp_emoji.items')
+  end
+
+  callback(self.items)
+end
+
+return source
+
